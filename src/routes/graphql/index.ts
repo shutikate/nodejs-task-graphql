0ce77efe3ee8e837ddcbaeb30396dd7ee8f6e595
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2024-10-29 15:04:13 +0100

    start project, add return qraphql

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { graphql } from 'graphql';
+import { schema } from './schema.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -15,7 +16,14 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      // return graphql();
+      const { query, variables } = req.body;
+      const response = await graphql({
+        schema,
+        source: query,
+        variableValues: variables,
+        contextValue: { prisma },
+      });
+      return response;
     },
   });
 };
---
+++ src/routes/graphql/query.ts
@@ -0,0 +1,5 @@
+import { GraphQLSchema } from "graphql";
+
+export const schema = new GraphQLSchema({
+
+});
